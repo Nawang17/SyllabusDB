e2b864a02275b6b457a151b23be4836f81f43961
Nawang17 <nawangofficial1010@gmail.com>
2025-07-18 12:57:35 -0400

    Update courseCode format in UploadSyllabus to include courseTitle for better clarity

--- src/pages/UploadSyllabusPage.jsx/UploadSyllabusPage.jsx
+++ src/pages/UploadSyllabusPage.jsx/UploadSyllabusPage.jsx
@@ -227,7 +227,7 @@ export default function UploadSyllabus() {
         },
         body: JSON.stringify({
           collegeName: collegeId,
-          courseCode: courseCode,
+          courseCode: `${courseCode} - ${courseTitle}`,
         }),
       });
     }
